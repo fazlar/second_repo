fdd98b84fb2f324b547cd08e20cd52ba3cb35048
unknown <fajlarabbi1991@gmail.com>
2021-01-15 22:27:45 +0600

    add java file

---
+++ File.java
@@ -0,0 +1,9 @@
+public class File {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        String name = reader.nextLine();
+        System.out.println("Hello!" + name);
+    }
+}
